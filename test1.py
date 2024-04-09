2c5e8a6ae653976cf9dbd367129f66408acf4dc7
wangtao <857219525qq@.com>
2024-04-09 16:33:06 +0800

    add test1.py

---
+++ test1.py
@@ -0,0 +1,11 @@
+import unittest
+class Test(unittest.TestCase):
+    def test_o1(self):
+        print("test_o1")
+    def test_o2(self):
+        print("test_o2")
+    def test_o3(self):
+        print("test_o3")
+
+if __name__ == '__main__':
+    unittest.main()
